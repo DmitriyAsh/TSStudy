e199db52ce319cce9f94e68dd28ca549645a286e
Dmitriy Dmitriev <dmitriyd1107@gmail.com>
2023-11-23 19:50:09 +0700

    add UserList component and types for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import Card, { CardVariant } from "./components/Card";
+import UserList from "./components/UserList";
 
 const App = () => {
+    const users = [
+        {
+            id: 1,
+            name: "Dmitry",
+            email: "asd@gmail.com",
+            address: { city: "Kemerovo", street: "Lenina", zipcode: "221133" },
+        },
+        {
+            id: 1,
+            name: "Po;ina",
+            email: "qwe@gmail.com",
+            address: {
+                city: "Kemerovo",
+                street: "Pushkina",
+                zipcode: "222233",
+            },
+        },
+    ];
+
     return (
         <div>
             <Card
@@ -11,6 +31,7 @@ const App = () => {
             >
                 <button>Button</button>
             </Card>
+            <UserList users={users} />
         </div>
     );
 };
